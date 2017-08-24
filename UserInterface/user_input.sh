a12e9c700c8f04eb79138eb1eaf642ee692f2af9
Kasun Dissanayake <kasunprageethdissanayake@gmail.com>
2017-08-24 18:59:24 +0530

    User Interface

---
+++ UserInterface/user_input.sh
@@ -0,0 +1,36 @@
+echo "--------Simple Login-------"
+echo " "
+read -p "UserID : " userid
+read -p "Password : " password
+echo " "
+echo "---------------------------"
+
+echo "======User Information Form========"
+echo " "
+read -p "First Name : " fname
+read -p "Last Name : " lname
+read -p "Date of Birth : " dob
+read -p "Telephone Number : " tel
+echo " "
+echo "===================================="
+
+echo "*********User**********"
+
+echo "----DATE----"
+date
+
+echo "Full name : " $fname $lname
+
+
+
+echo "Date of Birth : " $dob
+
+
+echo "Telephone Number : " $tel
+
+
+echo "********************************************************************"
+
+
+
+ll >> kasun.txt
